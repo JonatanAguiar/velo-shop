44e7ce5c258c568746aee8625f3079cdcbd95012
hcfava <59886550+hcfava@users.noreply.github.com>
2020-02-26 17:21:45 -0300

    Classe DAO adicionada

---
+++ src/br/com/veloweb/veloshop/modelo/dao/DAO.java
@@ -0,0 +1,111 @@
+package br.com.veloweb.veloshop.modelo.dao;
+
+import java.util.List;
+
+
+
+import javax.persistence.EntityManager;
+
+ 
+
+import br.com.veloweb.flyweb.connection.ConnectionFactory;
+
+ 
+
+public class DAO<E> {
+    
+    public E save(E obj, Integer objId) {
+        EntityManager em = new ConnectionFactory().getConnection();
+
+ 
+
+        try {
+            em.getTransaction().begin();
+            if (objId == null) {
+                em.persist(obj);
+            } else {
+                em.merge(obj);
+            }
+            em.getTransaction().commit();
+
+ 
+
+        } catch (Exception e) {
+            System.err.println(e);
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
+        return obj;
+    }
+
+ 
+
+    public E findById(Integer id, Class<E> x) {
+        EntityManager em = new ConnectionFactory().getConnection();
+
+ 
+
+        E obj = null;
+
+ 
+
+        try {
+            obj = em.find(x, id);
+        } catch (Exception e) {
+            System.err.println(e);
+        } finally {
+            em.close();
+        }
+
+ 
+
+        return obj;
+    }
+
+ 
+
+    public <T> List<T> findAll(String nome) {
+        EntityManager em = new ConnectionFactory().getConnection();
+        List<T> objs = null;
+
+ 
+
+        try {
+            objs = em.createQuery("from " + nome + " a").getResultList();
+        } catch (Exception e) {
+            System.err.println(e);
+        } finally {
+            em.close();
+        }
+
+ 
+
+        return objs;
+    }
+
+ 
+
+    public E remove(Integer id, Class<E> x) {
+        EntityManager em = new ConnectionFactory().getConnection();
+        E obj = null;
+
+ 
+
+        try {
+            obj = em.find(x, id);
+            em.getTransaction().begin();
+            em.remove(obj);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            System.err.println(e);
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
+
+ 
+
+        return obj;
+    }
+}
